3feda98309615e05f30a56d5312d81a6c7ba014e
Ece Kuru <ecekuru96@gmail.com>
2024-12-03 18:32:00 +0100

    Day 3 Part 2

--- 3/day3.py
+++ 3/day3.py
@@ -5,18 +5,29 @@ import re
 def mul(x,y):
     return x * y
 
-def part1():
-    regex = re.compile(r"mul\([0-9]+,[0-9]+\)")
+def part1(regex):
     result = 0
     with open("input.txt") as f:
         for line in f.readlines():
             matches = regex.findall(line)
             for match in matches:
-                print(match)
-                print(eval(match))
                 result += eval(match)
     print(f"Part 1: {result}")
 
+def part2(regex):
+    result = 0
+    with open("input.txt") as f:
+        fullinput = f.read()
+        sublines = fullinput.split("do()")
+        for subline in sublines:
+            partial = subline.split("don't()")[0]
+            matches = regex.findall(partial)
+            for match in matches:
+                result += eval(match)
+    print(f"Part 2: {result}")
+
 
 if __name__ == '__main__':
-    part1()
+    regex = re.compile(r"mul\([0-9]+,[0-9]+\)")
+    part1(regex)
+    part2(regex)
